d818497d98cf3ec7bda782173dee3fd432bb24b1
Quincy <165485409+souljaah@users.noreply.github.com>
2025-02-22 20:48:53 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/pages/home_screen.dart';
+import 'home_screen.dart';  // Import the HomeScreen
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),  // Set HomeScreen as the starting point
+    );
+  }
+}
 
